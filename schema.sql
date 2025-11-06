e854501c59452c1fc9c5790f47db8aa4a302e5f4
Reiss <Reiss02@gmail.com>
2025-11-06 13:42:24 +0000

    Created two schemas for the raw data and the clean data, which contains  imported csvs that have been loaded into tables

--- schema.sql
+++ schema.sql
@@ -1,17 +1,84 @@
-CREATE TABLE IF NOT EXISTS local_authority_districts_map (
+DROP SCHEMA IF EXISTS house_data CASCADE;
+DROP SCHEMA IF EXISTS raw_house_data CASCADE;
+CREATE SCHEMA IF NOT EXISTS house_data;
+CREATE SCHEMA IF NOT EXISTS raw_house_data;
+
+CREATE TABLE IF NOT EXISTS raw_house_data.local_authority_districts_map (
     Fid int NOT NULL UNIQUE,
     LAD23CD varchar(9) NOT NULL UNIQUE,
-    LAD23NM VARCHAR(32) NOT NULL UNIQUE,
-    LAD23NMW VARCHAR(32),
+    LAD23NM TEXT NOT NULL UNIQUE,
+    LAD23NMW TEXT,
     BNG_E int NOT NULL,
     BNG_N int NOT NULL,
-    LONG REAL NOT NULL,
-    LAT REAL NOT NULL,
-    Shape_Area REAL NOT NULL,
-    Shape_Length REAL NOT NULL,
+    LONG DOUBLE PRECISION NOT NULL,
+    LAT DOUBLE PRECISION NULL,
+    Shape_Area DOUBLE PRECISION NOT NULL,
+    Shape_Length DOUBLE PRECISION NOT NULL,
     GlobalID TEXT not null,
-    PRIMARY KEY("LAD23NM")
+    PRIMARY KEY(LAD23NM)
 );
 
-\copy local_authority_districts_map FROM 'local_authority_districts_map.csv' DELIMITER ',' csv HEADER;
+COPY raw_house_data.local_authority_districts_map FROM '/imports/local_authority_districts_map.csv' WITH DELIMITER ',' csv HEADER;
 
+CREATE TABLE house_data.local_authority_districts_map AS
+SELECT Fid,
+LAD23CD,
+UPPER(TRIM(LAD23NM)) AS LAD23NM,
+LAD23NMW,
+BNG_E,
+BNG_N,
+LONG,
+LAT,
+Shape_Area,
+Shape_Length,
+GlobalID
+FROM raw_house_data.local_authority_districts_map;
+
+ALTER TABLE house_data.local_authority_districts_map ADD PRIMARY KEY (LAD23NM);
+
+SET DateStyle TO 'European';
+
+CREATE TABLE IF NOT EXISTS raw_house_data.house_price_paid (
+    sale_id TEXT NOT NULL UNIQUE,
+    price INT NOT NULL,
+    "datetime" TEXT NOT NULL,
+    postcode VARCHAR(10),
+    property_type CHAR(1) NOT NULL,
+    new_build CHAR(1) NOT NULL,
+    tenure CHAR(1) NOT NULL,
+    PAON TEXT NOT NULL,
+    SAON TEXT,
+    street TEXT,
+    locality TEXT,
+    town_city TEXT NOT NULL,
+    district TEXT NOT NULL,
+    county TEXT NOT NULL,
+    PPD_category CHAR(1) NOT NULL,
+    record_status CHAR(1) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS house_data.house_price_paid (
+    sale_id TEXT NOT NULL UNIQUE,
+    price INT NOT NULL,
+    "date" DATE NOT NULL,
+    property_type CHAR(1) NOT NULL,
+    new_build BOOLEAN NOT NULL,
+    district TEXT NOT NULL,
+    county TEXT NOT NULL,
+    FOREIGN KEY(district) REFERENCES house_data.local_authority_districts_map (LAD23NM)
+);
+
+COPY raw_house_data.house_price_paid FROM '/imports/pp-2025.csv' DELIMITER ',' csv HEADER;
+
+INSERT INTO house_data.house_price_paid(sale_id, price, "date", property_type, new_build, district, county)
+SELECT sale_id,
+price,
+CAST("datetime" AS DATE) as "date",
+property_type,
+(new_build = 'Y') AS new_build,
+UPPER(TRIM(district)) AS district,
+county
+FROM raw_house_data.house_price_paid;
+
+DROP TABLE raw_house_data.house_price_paid;
+DROP TABLE raw_house_data.local_authority_districts_map;
