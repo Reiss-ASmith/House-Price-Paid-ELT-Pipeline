8a9f69229e47f262b8ed541a8c6c82dfef682454
Reiss <Reiss02@gmail.com>
2025-11-06 14:56:53 +0000

    creates a view for the total number of sales per district

--- views.sql
+++ views.sql
@@ -15,3 +15,10 @@ CREATE VIEW median_price_per_month AS
 SELECT EXTRACT('MONTH' FROM "date") AS "Month", ROUND(percentile_cont(0.5) WITHIN GROUP(ORDER BY price)::numeric, 2) AS median_price
 FROM house_data.house_price_paid
 GROUP BY EXTRACT('MONTH' FROM "date");
+
+DROP VIEW IF EXISTS total_number_of_sales_per_district;
+CREATE VIEW total_number_of_sales_per_district AS
+SELECT district, COUNT(*)
+FROM house_data.house_price_paid
+GROUP BY district;
+
