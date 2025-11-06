0ba0cc2e205e15cbbe4173bfffa40ab64757df46
Reiss <Reiss02@gmail.com>
2025-11-06 14:36:50 +0000

    created view for median price per county

--- views.sql
+++ views.sql
@@ -1,5 +1,12 @@
 DROP VIEW IF EXISTS median_price_per_district;
-CREATE VIEW IF NOT EXISTS median_price_per_district AS 
+CREATE VIEW median_price_per_district AS 
 SELECT district, ROUND(percentile_cont(0.5) WITHIN GROUP(ORDER BY price)::numeric, 2) AS median_price
 FROM house_data.house_price_paid
 GROUP BY district;
+
+DROP VIEW IF EXISTS median_price_per_county;
+CREATE VIEW median_price_per_county AS
+SELECT county, ROUND(percentile_cont(0.5) WITHIN GROUP(ORDER BY price)::numeric, 2) AS median_price
+FROM house_data.house_price_paid
+GROUP BY county;
+
