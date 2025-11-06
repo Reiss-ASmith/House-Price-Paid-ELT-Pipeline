a9cb4ee199300471aa9b2978cf5f487b75066449
Reiss <Reiss02@gmail.com>
2025-11-06 21:10:25 +0000

    created a new schema file in order to normalise the current database schema. I have also copied all of the raw schema tables into this file.

---
+++ normalised_schema.sql
@@ -0,0 +1,48 @@
+--clears the database
+DROP SCHEMA IF EXISTS house_data CASCADE;
+DROP SCHEMA IF EXISTS raw_house_data CASCADE;
+
+--creates schemas for data imports
+CREATE SCHEMA IF NOT EXISTS house_data;
+CREATE SCHEMA IF NOT EXISTS raw_house_data;
+
+-- raw_house_data schema creation
+--creates a temporary table used to store the raw data from the local_authority_district_map file later
+CREATE TABLE IF NOT EXISTS raw_house_data.local_authority_districts_map (
+    Fid int NOT NULL UNIQUE,
+    LAD23CD varchar(9) NOT NULL UNIQUE,
+    LAD23NM TEXT NOT NULL UNIQUE,
+    LAD23NMW TEXT,
+    BNG_E int NOT NULL,
+    BNG_N int NOT NULL,
+    LONG DOUBLE PRECISION NOT NULL,
+    LAT DOUBLE PRECISION NULL,
+    Shape_Area DOUBLE PRECISION NOT NULL,
+    Shape_Length DOUBLE PRECISION NOT NULL,
+    GlobalID TEXT not null,
+    PRIMARY KEY(LAD23NM)
+);
+
+--creates a temporary table used to store the raw data from the house price paid files files later
+CREATE TABLE IF NOT EXISTS raw_house_data.house_price_paid (
+    sale_id TEXT NOT NULL UNIQUE,
+    price INT NOT NULL,
+    "datetime" TEXT NOT NULL,
+    postcode VARCHAR(10),
+    property_type CHAR(1) NOT NULL,
+    new_build CHAR(1) NOT NULL,
+    tenure CHAR(1) NOT NULL,
+    PAON TEXT NOT NULL,
+    SAON TEXT,
+    street TEXT,
+    locality TEXT,
+    town_city TEXT NOT NULL,
+    district TEXT NOT NULL,
+    county TEXT NOT NULL,
+    PPD_category CHAR(1) NOT NULL,
+    record_status CHAR(1) NOT NULL
+);
+
+--imports files into tables above
+COPY raw_house_data.local_authority_districts_map FROM '/imports/local_authority_districts_map.csv' WITH DELIMITER ',' csv HEADER;
+COPY raw_house_data.house_price_paid FROM '/imports/pp-2025.csv' DELIMITER ',' csv HEADER;
