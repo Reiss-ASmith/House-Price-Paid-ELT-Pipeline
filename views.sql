070d2f5369c3f88ce5935eae645b4e25b90b583e
Reiss <Reiss02@gmail.com>
2025-11-08 11:33:40 +0000

    created a view that shows the median price per year

--- views.sql
+++ views.sql
@@ -26,8 +26,8 @@ FROM house_data.house_price_paid_2025 AS p
 JOIN house_data.districts AS d ON d.district_id = p.district_id
 GROUP BY district;
 
-DROP VIEW IF EXISTS total_sales_per_county;
-CREATE VIEW total_sales_per_county AS
+DROP VIEW IF EXISTS total_sales_per_county_2025;
+CREATE VIEW total_sales_per_county_2025 AS
 SELECT county, COUNT(*) AS total_house_sales
 FROM house_data.house_price_paid_2025 AS p
 JOIN house_data.districts AS d ON d.district_id = p.district_id
@@ -39,3 +39,9 @@ CREATE VIEW total_sales_per_month_2025 AS
 SELECT EXTRACT('MONTH' FROM "date") AS "Month", COUNT(*) AS total_house_sales
 FROM house_data.house_price_paid_2025
 GROUP BY EXTRACT('MONTH' FROM "date");
+
+DROP VIEW IF EXISTS median_price_per_year;
+CREATE VIEW median_price_per_year AS
+SELECT EXTRACT('YEAR' FROM "date") AS "Year", ROUND(percentile_cont(0.5) WITHIN GROUP(ORDER BY price)::numeric, 2) AS median_price
+FROM house_data.house_price_paid
+GROUP BY EXTRACT('YEAR' FROM "date");
