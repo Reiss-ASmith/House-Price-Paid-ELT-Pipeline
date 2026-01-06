1647bdd99dcc77712a71cce294fde73f8f1c12cf
Reiss-ASmith <reiss02@gmail.com>
2026-01-06 17:45:47 +0000

    added comments that explain queries in 03 file

--- 03_transformation_views.sql
+++ 03_transformation_views.sql
@@ -1,3 +1,4 @@
+--Creates view for the median price of a house sold in each district every year
 CREATE OR REPLACE VIEW house_data.v_median_price_per_district_year AS
 SELECT
 d.lad23cd,
@@ -8,6 +9,7 @@ FROM house_data.house_price_paid p
 JOIN house_data.districts d ON d.district_id = p.district_id
 GROUP BY d.lad23cd, d.district, EXTRACT(YEAR FROM p."date");
 
+--Creates view for the median price of a house sold in each county every year
 CREATE OR REPLACE VIEW house_data.v_median_price_per_county_year AS
 SELECT
 c.county,
@@ -18,6 +20,7 @@ JOIN house_data.districts d ON d.district_id = p.district_id
 JOIN house_data.counties c ON c.county_id = d.county_id
 GROUP BY c.county, EXTRACT(YEAR FROM p."date");
 
+--Creates view that shows the median price per month of each year
 CREATE OR REPLACE VIEW house_data.v_median_price_per_month_year AS
 SELECT
 EXTRACT(YEAR FROM p."date")::int  AS year,
@@ -26,6 +29,7 @@ ROUND(percentile_cont(0.5) WITHIN GROUP (ORDER BY p.price)::numeric, 2) AS media
 FROM house_data.house_price_paid p
 GROUP BY EXTRACT(YEAR FROM p."date"), EXTRACT(MONTH FROM p."date");
 
+--Creates a view that shows the total number of sales per district each year
 CREATE OR REPLACE VIEW house_data.v_total_sales_per_district_year AS
 SELECT
 d.lad23cd,
@@ -36,6 +40,7 @@ FROM house_data.house_price_paid p
 JOIN house_data.districts d ON d.district_id = p.district_id
 GROUP BY d.lad23cd, d.district, EXTRACT(YEAR FROM p."date");
 
+--Creates a view that shows the total number of house sales per county each year
 CREATE OR REPLACE VIEW house_data.v_total_sales_per_county_year AS
 SELECT
 c.county,
@@ -46,6 +51,7 @@ JOIN house_data.districts d ON d.district_id = p.district_id
 JOIN house_data.counties c ON c.county_id = d.county_id
 GROUP BY c.county, EXTRACT(YEAR FROM p."date");
 
+--Creates a view that shows the median house sale price per year across England & Wales
 CREATE OR REPLACE VIEW house_data.v_median_price_per_year AS
 SELECT
 EXTRACT(YEAR FROM p."date")::int AS year,
