69ed4f2b392b731102a00fd62953835a03c3b7ee
Reiss-ASmith <reiss02@gmail.com>
2026-01-07 19:52:54 +0000

    added csv url to extract.py using the requsts library

--- 01_extract.py
+++ 01_extract.py
@@ -1,3 +1,7 @@
+import sys
 import requests
 import pandas as pd
-import numpy as np
+
+url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv"
+
+response = requests.get(url, stream=True)
