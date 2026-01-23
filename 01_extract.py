8ad029e75310c2afae795f986a525919b26392e2
Reiss-ASmith <reiss02@gmail.com>
2026-01-23 17:12:48 +0000

    created a function in 01_extract.py that opens a streaming HTTP connection to the Land Registry complete csv file

--- 01_extract.py
+++ 01_extract.py
@@ -1,7 +1,12 @@
-import sys
 import requests
-import pandas as pd
+from db import get_connection
+from contextlib import contextmanager
 
-url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv"
+complete_data_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv"
 
-response = requests.get(url, stream=True)
+@contextmanager
+def get_complete_data_stream():
+    with requests.get(complete_data_url, stream=True, timeout=120) as response:
+        response.status_code
+        yield response
+
